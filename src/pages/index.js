788f95209c76f55a51fc925490c0387db94d90de
ArshdeepSingh2267 <arsh160100@gmail.com>
2024-02-16 11:53:26 +0530

    edit baseURL for foodData

--- src/pages/index.js
+++ src/pages/index.js
@@ -98,7 +98,11 @@ export default function Home(props) {
 }
 
 export async function getStaticProps() {
-  const pizzaData = await fetch("/api/foodData", {
+  const baseURL =
+    process.env.NODE_ENV === "production"
+      ? "https://pizzawizza.vercel.app/"
+      : "http://127.0.0.1:3000/";
+  const pizzaData = await fetch(baseURL + "api/foodData", {
     method: "GET",
   });
   let data = await pizzaData.json();
